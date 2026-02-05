44edadf6a6ff27d1bc98a94fccd8553a7e36671d
Abdirahim Omar Hassan <abdirahimomar147@gmail.com>
2026-02-05 14:56:16 +0300

    efarming web

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -10,11 +9,11 @@ const nextConfig: NextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
-        port: "3000",
+        port: "3001",
         pathname: "/uploads/**",
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
